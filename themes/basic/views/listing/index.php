472ec9773b77ce09e2cf9da2163b6e81c72757c8
araza <araza@tradetracker.com>
2016-10-16 22:19:07 +0400

    update markup to bootstrap 4 for listing view

--- themes/basic/views/listing/index.php
+++ themes/basic/views/listing/index.php
@@ -5,43 +5,42 @@ use app\widgets\RecentListingsWidget;
 use app\widgets\BannerWidget;
 $this->title = $heading = isset($_GET['category']) ? ucwords($_GET['category']) : 'Result';
 ?>
-
 <div class="row">
-	<aside class="sidebar sidebar--left col-lg-3">
-		<div class="filter">
-			<h3 class="filter__heading">Category</h3>
-		</div>
-	</aside>
-	<!-- /sidebar -->
+    <aside class="sidebar sidebar--left col-lg-3">
+        <div class="filter">
+            <h3 class="filter__heading">Category</h3>
+        </div>
+    </aside>
+    <!-- /sidebar -->
 
-	<main class="main col-lg-9">
-		<div class="listing-group listing-group--listview">
-			<h3 class="listing-group__heading"><?= $heading ?></h3>
-			<div class="listing-group__body row">
-			    <?php foreach ($listings as $l): ?>
-			    <div class="col-lg-4">
-			        <div class="card card--listing">
-			        	<div class="card-media">
-			            	<img class="card-img-top" src="<?= $l->thumbSrc ?>" alt="<?= $l->title ?>" />
-			            </div>
-			            <div class="card-block">
-			                <h3 class="card-title">
-			                    <?= a($l->title, $l->viewUrl, [
-			                        'title' => $l->title,
-			                        'class' => 'card-link'
-			                    ]) ?>
-			                </h3>
-			                <p class="card-text"><?= e(truncate($l->description)) ?></p>
-			                <p class="card-meta">
-			                    <?= t('app', 'Added at') ?>: <?= time_ago($l->created_at) ?>
-			                </p>
-			            </div>
-			        </div>
-			    </div>
-			    <?php endforeach ?>
-		    </div>
-		</div>
-		<!-- /listing-list -->
-	</main>
-	<!-- /main -->
+    <main class="main col-lg-9">
+        <div class="listing-group listing-group--listview">
+            <h3 class="listing-group__heading"><?= $heading ?></h3>
+            <div class="listing-group__content row">
+                <?php foreach ($listings as $l): ?>
+                <div class="col-lg-4">
+                    <div class="card card--listing">
+                        <div class="card-media">
+                            <img class="card-img-top" src="<?= $l->thumbSrc ?>" alt="<?= $l->title ?>" />
+                        </div>
+                        <div class="card-block">
+                            <h3 class="card-title">
+                                <?= a($l->title, $l->viewUrl, [
+                                    'title' => $l->title,
+                                    'class' => 'card-link'
+                                ]) ?>
+                            </h3>
+                            <p class="card-text"><?= e(truncate($l->description)) ?></p>
+                            <p class="card-meta">
+                                <?= t('app', 'Added at') ?>: <?= time_ago($l->created_at) ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <?php endforeach ?>
+            </div>
+        </div>
+        <!-- /listing-list -->
+    </main>
+    <!-- /main -->
 </div>
--- themes/basic/views/listing/view.php
+++ themes/basic/views/listing/view.php
@@ -12,223 +12,222 @@ $this->title = $listing->title;
 $listingUrl = $listing->getViewUrl(true);
 ?>
 <section class="listing-view">
-	<header class="card listing-view__header well">
-		<div class="card-block">
-			<div class="action pull-sm-right">
-				<a href="#" class="action__link">
-					<span class="action__icon glyphicon glyphicon-heart-empty"></span>
-					Watch
-				</a>
-				<a href="#" class="action__link">
-					<span class="action__icon glyphicon glyphicon-duplicate"></span>
-					Post a similar ad
-				</a>
-			</div>
+    <header class="card listing-view__header">
+        <div class="card-block">
+            <div class="listing-action pull-lg-right">
+                <a href="#" class="listing-action__link">
+                    <span class="listing-action__icon glyphicon glyphicon-heart-empty"></span> Watch
+                </a>
+                <a href="#" class="listing-action__link">
+                    <span class="listing-action__icon glyphicon glyphicon-duplicate"></span> Post a similar ad
+                </a>
+            </div>
 
-			<h1 class="listing-view__title"><?= e($listing->title) ?></h1>
-			<?php if (!empty($listing->price) && $listing->price > 0): ?>
-				<h5 class="listing-view__price">$<?= $listing->price ?> <?= $listing->price_type ?></h5>
-			<?php endif ?>
-			<?php if ($listing->address): ?>
-				<p class="listing-view__address">
-					<span class="glyphicon glyphicon-map-marker"></span>
-					<?= $listing->address ?>
-				</p>
-			<?php endif ?>
-		</div>
-	</header>
+            <h1 class="listing-view__title"><?= e($listing->title) ?></h1>
+            <!-- @TODO: move price check to listing model -->
+            <?php if (!empty($listing->price) && $listing->price > 0): ?>
+            <h5 class="listing-view__price">$<?= $listing->price ?> <?= $listing->price_type ?></h5>
+            <?php endif ?>
 
-	<div class="row">
-		<div class="listing-view__content col-lg-8">
-			<div class="gallery">
-				<div class="gallery__preview">
-					<img class="gallery__image" src="<?= $listing->thumbSrc ?>" alt=""/>
-				</div>
-				<div class="gallery__thumbs">
-				<?php foreach ($listing->images as $image): ?>
-					<img class="gallery__thumb" src="<?= $image->thumbSrc ?>" width="120" height="90" />
-				<?php endforeach ?>
-				</div>
-			</div>
-			<!-- /gallery -->
+            <?php if ($listing->address): ?>
+            <p class="listing-view__address">
+                <span class="glyphicon glyphicon-map-marker"></span>
+                <small class="text-muted"><?= $listing->address ?></small>
+            </p>
+            <?php endif ?>
+        </div>
+    </header>
+    <!-- /header -->
 
-			<div class="row">
-				<div class="col-lg-6">
-					<table class="table attribute-list">
-						<tbody>
-							<tr>
-								<th><?= t('app', 'Category') ?></th>
-								<td><?= $listing->category ?></td>
-							</tr>
-							<tr>
-								<th><?= t('app', 'Published') ?></th>
-								<td><?= time_ago($listing->created_at) ?></td>
-							</tr>
-							<?php if (!empty($listing->website_url)): ?>
-							<tr>
-								<th><?= t('app', 'Web Site') ?></th>
-								<td>
-									<?= a($listing->website_url, $listing->website_url, [
-										'target' => '_blank',
-										'rel' => 'nofollow'
-									]) ?>
-								</td>
-							</tr>
-							<?php endif ?>
-							<?php if (!empty($listing->expiry_date)): ?>
-								<tr>
-									<th><?= t('app', 'Listing Valid Until')?></th>
-									<td><?= $listing->expiry_date ?></td>
-								</tr>
-							<?php endif ?>
-						</tbody>
-					</table>
-				</div>
-				<div class="col-lg-6">
-					<table class="table attribute-list">
-						<tbody>
-							<tr>
-								<th><?= t('app', 'Category') ?></th>
-								<td><?= $listing->category ?></td>
-							</tr>
-							<tr>
-								<th><?= t('app', 'Published') ?></th>
-								<td><?= time_ago($listing->created_at) ?></td>
-							</tr>
-							<?php if (!empty($listing->website_url)): ?>
-							<tr>
-								<th><?= t('app', 'Web Site') ?></th>
-								<td>
-									<?= a($listing->website_url, $listing->website_url, [
-										'target' => '_blank',
-										'rel' => 'nofollow'
-									]) ?>
-								</td>
-							</tr>
-							<?php endif ?>
-							<?php if (!empty($listing->expiry_date)): ?>
-								<tr>
-									<th><?= t('app', 'Listing Valid Until')?></th>
-									<td><?= $listing->expiry_date ?></td>
-								</tr>
-							<?php endif ?>
-						</tbody>
-					</table>
-				</div>
-			</div>
-			<!-- /listing-view__details -->
+    <div class="row">
+        <div class="listing-view__content col-lg-8">
+            <div class="gallery">
+                <div class="gallery__preview">
+                    <img class="gallery__image" src="<?= $listing->thumbSrc ?>" alt=""/>
+                </div>
+                <div class="gallery__thumbs">
+                <?php foreach ($listing->images as $image): ?>
+                    <img class="gallery__thumb" src="<?= $image->thumbSrc ?>" width="120" height="90" />
+                <?php endforeach ?>
+                </div>
+            </div>
+            <!-- /gallery -->
 
-			<div class="card card-default listing-view__description">
-				<div class="card-block">
-					<?= nl2br(e($listing->description)) ?>
-				</div>
-			</div>
-			<!-- /listing-view__description -->
+            <div class="row">
+                <div class="col-lg-6">
+                    <table class="table attribute-list">
+                        <tbody>
+                            <tr>
+                                <th><?= t('app', 'Category') ?></th>
+                                <td><?= $listing->category ?></td>
+                            </tr>
+                            <tr>
+                                <th><?= t('app', 'Published') ?></th>
+                                <td><?= time_ago($listing->created_at) ?></td>
+                            </tr>
+                            <?php if (!empty($listing->website_url)): ?>
+                            <tr>
+                                <th><?= t('app', 'Web Site') ?></th>
+                                <td>
+                                    <?= a($listing->website_url, $listing->website_url, [
+                                        'target' => '_blank',
+                                        'rel' => 'nofollow'
+                                    ]) ?>
+                                </td>
+                            </tr>
+                            <?php endif ?>
+                            <?php if (!empty($listing->expiry_date)): ?>
+                            <tr>
+                                <th><?= t('app', 'Listing Valid Until')?></th>
+                                <td><?= $listing->expiry_date ?></td>
+                            </tr>
+                            <?php endif ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-lg-6">
+                    <table class="table attribute-list">
+                        <tbody>
+                            <tr>
+                                <th><?= t('app', 'Category') ?></th>
+                                <td><?= $listing->category ?></td>
+                            </tr>
+                            <tr>
+                                <th><?= t('app', 'Published') ?></th>
+                                <td><?= time_ago($listing->created_at) ?></td>
+                            </tr>
+                            <?php if (!empty($listing->website_url)): ?>
+                            <tr>
+                                <th><?= t('app', 'Web Site') ?></th>
+                                <td>
+                                    <?= a($listing->website_url, $listing->website_url, [
+                                        'target' => '_blank',
+                                        'rel' => 'nofollow'
+                                    ]) ?>
+                                </td>
+                            </tr>
+                            <?php endif ?>
+                            <?php if (!empty($listing->expiry_date)): ?>
+                                <tr>
+                                    <th><?= t('app', 'Listing Valid Until')?></th>
+                                    <td><?= $listing->expiry_date ?></td>
+                                </tr>
+                            <?php endif ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- /listing-view__details -->
 
-			<?php // BannerWidget::widget(['position' => '720x90']) ?>
-			<?= SimilarListingsWidget::widget(['listing' => $listing]) ?>
-		</div>
-		<!-- /listing-view__content -->
+            <div class="card m-b-3 listing-view__description">
+                <div class="card-block">
+                    <?= nl2br(e($listing->description)) ?>
+                </div>
+            </div>
+            <!-- /listing-view__description -->
 
-		<div class="listing-view__sidebar col-lg-4">
-			<div class="clearfix">
-				<div class="report pull-sm-right">
-		            <a href="#" class="report__link">
-		                <span class="report__icon glyphicon glyphicon-flag"></span>
-		                <span class="report__label">Report</span>
-		            </a>
-	            </div>
-	            <!-- /report -->
+            <?php // BannerWidget::widget(['position' => '720x90']) ?>
+            <?= SimilarListingsWidget::widget(['listing' => $listing]) ?>
+        </div>
+        <!-- /listing-view__content -->
 
-				<div class="share">
-					<div class="share__link share__link--facebook">
-						<a href="#" target="_blank"><span>Share on Facebook</span></a>
-					</div>
-					<div class="share__link share__link--twitter">
-						<a href="#" target="_blank"><span>Tweet</span></a>
-					</div>
-					<div class="share__link share__link--google-plus">
-						<a href="#"><span>Share on Google+</span></a>
-					</div>
-					<div class="share__link share__link--pinterest">
-						<a href="#"  target="_blank"><span>Pin</span></a>
-					</div>
-	                <div class="share__link share__link--email">
-	                    <a id="ad-share-friend-link" href="#"><span>Share with a friend</span></a>
-	                </div>
-				</div>
-				<!-- /share -->
-			</div>
+        <div class="listing-view__sidebar col-lg-4">
+            <div class="clearfix m-b-1">
+                <div class="report pull-lg-right">
+                    <a href="#" class="report__link">
+                        <span class="report__icon glyphicon glyphicon-flag"></span>
+                        <span class="report__label">Report</span>
+                    </a>
+                </div>
+                <!-- /report -->
 
-			<br>
+                <div class="share">
+                    <div class="share__link share__link--facebook">
+                        <a href="#" target="_blank"><span>Share on Facebook</span></a>
+                    </div>
+                    <div class="share__link share__link--twitter">
+                        <a href="#" target="_blank"><span>Tweet</span></a>
+                    </div>
+                    <div class="share__link share__link--google-plus">
+                        <a href="#"><span>Share on Google+</span></a>
+                    </div>
+                    <div class="share__link share__link--pinterest">
+                        <a href="#"  target="_blank"><span>Pin</span></a>
+                    </div>
+                    <div class="share__link share__link--email">
+                        <a id="ad-share-friend-link" href="#"><span>Share with a friend</span></a>
+                    </div>
+                </div>
+                <!-- /share -->
+            </div>
 
-			<div class="card card--user">
-				<div class="card-block">
-					<?php if (!empty($listing->user)): ?>
-					<div class="user clearfix">
-						<div class="user__media pull-sm-left">
-							<span class="user__icon glyphicon glyphicon-user"></span>
-						</div>
-						<div class="user__info pull-sm-left">
-							<strong class="user__name"><?= $listing->user ?></strong>
-							<time class="user__member-since">
-								Member since <?= date('Y', $listing->user->created_at) ?>
-							</time>
-						</div>
-					</div>
-					<!-- /user -->
-					<?php endif ?>
-					<div class="reply">
-						<a href="#" class="btn btn-lg btn-primary u-block">Send Message</a>
-						<?php if ($listing->user): ?>
-							<a href="#">
-								<?= e($listing->user->phone) ?>
-							</a>
-						<?php endif ?>
-						<?php if ($listing->user): ?>
-							<a href="skype:<?= $listing->user->skype ?>?chat">
-								<?= e($listing->user->skype) ?>
-							</a>
-						<?php endif ?>
-					</div>
-					<!-- /reply -->
-				</div>
-			</div>
-			<!-- /card -->
+            <div class="card card--user">
+                <div class="card-block">
+                    <?php if (!empty($listing->user)): ?>
+                    <div class="user clearfix">
+                        <div class="user__media pull-lg-left">
+                            <span class="user__icon"></span>
+                        </div>
+                        <div class="user__info pull-lg-left">
+                            <strong class="user__name"><?= $listing->user ?></strong>
+                            <time class="user__member-since">
+                                Member since <?= date('Y', $listing->user->created_at) ?>
+                            </time>
+                        </div>
+                    </div>
+                    <!-- /user -->
+                    <?php endif ?>
+                    <div class="reply">
+                        <a href="#" class="btn btn-primary btn-lg u-block">Send Message</a>
+                        <?php if ($listing->user): ?>
+                        <a href="#">
+                            <?= e($listing->user->phone) ?>
+                        </a>
+                        <?php endif ?>
+                        <?php if ($listing->user): ?>
+                        <a href="skype:<?= $listing->user->skype ?>?chat">
+                            <?= e($listing->user->skype) ?>
+                        </a>
+                        <?php endif ?>
+                    </div>
+                    <!-- /reply -->
+                </div>
+            </div>
+            <!-- /card -->
 
-			<div class="safety clearfix">
-				<div class="safety__media pull-left">
-					<span class="safety__icon glyphicon glyphicon-ok-sign"></span>
-				</div>
-				<div class="safety__info">
-					<p class="safety__tip">
-						Always meet the seller face-to-face to inspect and test drive the car. 
-					</p>
-					<a href="#" class="safety__link">Find more helpful hints here</a>
-				</div>
-			</div>
-			<!-- /safety -->
+            <div class="safety clearfix">
+                <div class="safety__media pull-lg-left">
+                    <span class="safety__icon glyphicon glyphicon-ok-sign"></span>
+                </div>
+                <div class="safety__info">
+                    <p class="safety__tip">
+                        Always meet the seller face-to-face to inspect and test drive the car. 
+                    </p>
+                    <a href="#" class="safety__link">Find more helpful hints here</a>
+                </div>
+            </div>
+            <!-- /safety -->
 
-		</div>
-		<!-- /listing-view__sidebar -->
-	</div>
-		
-	<?php if (!empty($listing->video)): ?>
-	<div class="listing-view__video">
-		<?= video_player($video) ?>
-	</div>
-	<?php endif ?>
+        </div>
+        <!-- /listing-view__sidebar -->
+    </div>
+        
+    <?php if (!empty($listing->video)): ?>
+    <div class="listing-view__video">
+        <?= video_player($video) ?>
+    </div>
+    <?php endif ?>
 
-	<?php // $this->render('_message_form', ['model' => $contactModal]) ?>
+    <?php // $this->render('_message_form', ['model' => $contactModal]) ?>
 
-	<?= GoogleMapWidget::widget([
-		'coordinates' => $listing->coordinates,
-		'address' => $listing->address
-	]) ?>
+    <?= GoogleMapWidget::widget([
+        'coordinates' => $listing->coordinates,
+        'address' => $listing->address
+    ]) ?>
 
-	<div class="listing-view__tags">
-		<?= TagsWidget::widget(['tags' => $listing->tags]) ?>
-	</div>
-	<!-- /listing-view__tags -->
+    <div class="listing-view__tags">
+        <?= TagsWidget::widget(['tags' => $listing->tags]) ?>
+    </div>
+    <!-- /listing-view__tags -->
 </section>
 <!-- /listing-view -->
